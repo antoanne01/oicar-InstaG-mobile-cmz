77e4784988f31c0816d12e1b7c0ebd25d625c91b
antoanne01 <covic_a.20@protonmail.com>
2024-04-02 16:11:44 +0200

    Initial page (opening)

--- app/src/main/java/com/ac/ktandroidapps/oicar_instag_mobile_cmz/MainActivity.kt
+++ app/src/main/java/com/ac/ktandroidapps/oicar_instag_mobile_cmz/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.ac.ktandroidapps.oicar_instag_mobile_cmz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if(FirebaseAuth.getInstance().currentUser == null){
+                startActivity(Intent(this, SignUpActivity::class.java))
+            }
+            else{
+                startActivity(Intent(this, SignUpActivity::class.java))
+                finish()
+            }
+        }, 2000)
     }
 }
---
+++ app/src/main/java/com/ac/ktandroidapps/oicar_instag_mobile_cmz/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.ac.ktandroidapps.oicar_instag_mobile_cmz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
